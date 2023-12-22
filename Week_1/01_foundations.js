531790f4d2371898b5109969f7e8d7ae8e00a53c
Prathamesh Powar <ppowar1910@gmail.com>
2023-12-22 21:18:17 +0530

    Add name and age variables

--- Week_1/01_foundations.js
+++ Week_1/01_foundations.js
@@ -4,3 +4,15 @@ setInterval(()=>{
   const time = new Date();
   console.log(time.toLocaleTimeString());
 },1000)
+
+// 1.1
+// Create a variable called `name` and assign it to your name as a string
+// Log the variable to the console
+const name = "Amanda";
+console.log(name);
+
+// 1.2
+// Create a variable called `age` and assign it your age as a number
+// Log the variable to the console
+const age = 25;
+console.log(age);
