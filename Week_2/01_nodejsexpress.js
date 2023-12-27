85c3f3dbd1638beebf60c41b3f7e59794f4224ca
Prathamesh Powar <ppowar1910@gmail.com>
2023-12-27 22:44:02 +0530

    Added explanations about JavaScript and ECMAScript

---
+++ Week_2/01_nodejsexpress.js
@@ -0,0 +1,30 @@
+//? what is ecma script?
+// these are the rules that javascript follows
+
+//? what is javascript?
+// javascript is a programming language that is used to make web pages interactive
+// it is a scripting language
+// it is a high level language
+// it is a dynamic language
+// it is a weakly typed language
+// it is a prototype based language
+// it is a multi-paradigm language
+// it is a single threaded language
+// it is a interpreted language
+// it is a case sensitive language
+// it is a client side language
+// it is a server side language
+// it is a event driven language
+// it is a asynchronous language
+// it is a concurrent language
+// it is a distributed language
+// it is a functional language
+// it is a imperative language
+// it is a structured language
+// it is a object oriented language
+// it is a reflective language
+// it is a garbage collected language
+
+//? what is the difference between javascript and ecmascript?
+// javascript is a programming language while ecmascript is a set of rules that javascript follows
+
