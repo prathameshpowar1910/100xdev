5c6a88e0fec6858ef1593b6607673c209b46c1f3
Prathamesh Powar <ppowar1910@gmail.com>
2024-03-03 23:10:45 +0530

    Add React components and configuration files

---
+++ Week 6/class-01/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+     <Header title="Hello World" />
+     <Header title="Its prathamesh" />
+    </>
+  )
+}
+
+export default App
---
+++ Week 6/class-01/src/components/Header.jsx
@@ -0,0 +1,6 @@
+
+export default function Header({title}) {
+  return (
+    <div>The title is {title}</div>
+  )
+}
