9b9dc91f3a9e476567b42e2555637e4b8c1a4f97
Prathamesh Powar <ppowar1910@gmail.com>
2024-03-05 22:51:03 +0530

    Add useState hook and change title functionality in App.jsx, add PropTypes in Header.jsx

--- Week 6/class-01/src/App.jsx
+++ Week 6/class-01/src/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import './App.css'
 import Header from './components/Header'
 
 function App() {
 
+  const name = 'Raman';
+  const [random,setRandom] = useState('Hkirat')
+
+  const changeTitle = () => {
+    const random = Math.random(); 
+    setRandom(`${random}`)
+  }
+
   return (
     <>
-     <Header title="Hello World" />
-     <Header title="Its prathamesh" />
+     <button onClick={changeTitle} >Click me to change title</button>
+     <Header title={random} />
+     <Header title={name} />
     </>
   )
 }
 
-export default App
+export default App;
--- Week 6/class-01/src/components/Header.jsx
+++ Week 6/class-01/src/components/Header.jsx
@@ -1,6 +1,13 @@
 
+import PropTypes from 'prop-types';
+
 export default function Header({title}) {
+  
   return (
     <div>The title is {title}</div>
   )
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+};
