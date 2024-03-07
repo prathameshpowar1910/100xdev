986b00278dd7b02532b1b2afd8c09e487da18f21
Prathamesh Powar <ppowar1910@gmail.com>
2024-03-07 23:38:34 +0530

    Refactor Header component to use memo

--- Week 6/class-01/src/components/Header.jsx
+++ Week 6/class-01/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
+import { memo } from 'react';
 
-export default function Header({title}) {
-  
+const Header = memo(function Header({ title }) { //! memo is a higher order component that will prevent the component from re-rendering if the props have not changed
   return (
-    <div>The title is {title}</div>
-  )
-}
+    <div>The title is: {title}</div>
+  );
+});
 
 Header.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 };
+
+export default Header;
