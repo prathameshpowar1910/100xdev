94f7609d24ebb561073124044e029b49dbc326e2
Prathamesh Powar <ppowar1910@gmail.com>
2024-03-09 00:12:33 +0530

    Refactor App component and add CardWrapper component

--- Week 6/class-01/src/App.jsx
+++ Week 6/class-01/src/App.jsx
@@ -11,54 +11,88 @@ import { useState } from 'react'
 //     </>
 //   )
 // }
-let counter = 4; 
+// let counter = 4; 
+
+// function App() {
+//   const [todos,setTodos] = useState([
+//     {id:1, title: 'First todo', description: 'This is the first todo'},
+//     {id:2, title: 'Second todo', description: 'This is the second todo'},
+//     {id:3, title: 'Third todo', description: 'This is the third todo'},
+//   ])
+
+//   const [newTitle, setNewTitle] = useState('')
+//   const [newDescription, setNewDescription] = useState('')
+
+//   const addTodo = () => {
+//     setTodos([...todos, {id:counter++, title: newTitle, description: newDescription}])
+//   }
+
+//   return (
+//     <>
+//     <input type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} />
+//     <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} />
+//     <button onClick={addTodo}>Add todo</button>
+//     <button onClick={() => {
+//       setTodos([])
+//     }}>Clear todos</button>
+//     <h1>Find your todos below</h1>
+//     {todos.map((todo) => {
+//       return <Todo key={todo.id} id={todo.id} title={todo.title} description={todo.description} />
+//     })}
+//      {todos.length === 0 && 'No todos found'}
+//     </>
+//   )
+// }
+
+// import PropTypes from 'prop-types';
+
+// function Todo({id, title, description}) {
+//   return (
+//     <>
+//       <h3>{id}</h3>
+//       <h1>{title}</h1>
+//       <p>{description}</p>
+//     </>
+//   )
+// }
+
+// Todo.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   title: PropTypes.string.isRequired,
+//   description: PropTypes.string.isRequired
+// };
 
 function App() {
-  const [todos,setTodos] = useState([
-    {id:1, title: 'First todo', description: 'This is the first todo'},
-    {id:2, title: 'Second todo', description: 'This is the second todo'},
-    {id:3, title: 'Third todo', description: 'This is the third todo'},
-  ])
-
-  const [newTitle, setNewTitle] = useState('')
-  const [newDescription, setNewDescription] = useState('')
-
-  const addTodo = () => {
-    setTodos([...todos, {id:counter++, title: newTitle, description: newDescription}])
-  }
-
   return (
     <>
-    <input type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} />
-    <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} />
-    <button onClick={addTodo}>Add todo</button>
-    <button onClick={() => {
-      setTodos([])
-    }}>Clear todos</button>
-    <h1>Find your todos below</h1>
-    {todos.map((todo) => {
-      return <Todo key={todo.id} id={todo.id} title={todo.title} description={todo.description} />
-    })}
-     {todos.length === 0 && 'No todos found'}
+    <CardWrapper>
+      hello world
+    </CardWrapper>
+    {/* <CardWrapper innerComponent={<TextComponent />} /> */}
     </>
   )
 }
 
-import PropTypes from 'prop-types';
-
-function Todo({id, title, description}) {
+function CardWrapper({children}) {
   return (
-    <>
-      <h3>{id}</h3>
-      <h1>{title}</h1>
-      <p>{description}</p>
-    </>
+    <div style={{border:"2px solid black"}}>
+      {children}
+    </div>
   )
 }
 
-Todo.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
-};
+// function TextComponent() {
+//   return (
+//     <h1>Hello world </h1>
+//   )
+// }
+
+// function CardWrapper({innerComponent}) {
+//   return (
+//     <div style={{border:"2px solid black"}}>
+//       {innerComponent}
+//     </div>
+//   )
+// }
 
 export default App;
