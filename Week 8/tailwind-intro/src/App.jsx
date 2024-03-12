a0af2c561376fc0367fa576f33aa503ddf8ca511
Prathamesh Powar <ppowar1910@gmail.com>
2024-03-12 20:27:32 +0530

    Add Tailwind CSS configuration and files

---
+++ Week 8/tailwind-intro/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-600 font-bold underline text-center col">Hello world!</h1> 
+    </>
+  )
+}
+
+export default App
