c18b46e507e4e4e936f489a067eb3f3bfbc793be
Prathamesh Powar <ppowar1910@gmail.com>
2024-03-21 12:53:48 +0530

    custome hooks useTodos

--- Week 9/custom-hooks/src/App.jsx
+++ Week 9/custom-hooks/src/App.jsx
@@ -1,16 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useEffect, useState } from 'react'
+// import './App.css'
+
+// function App() {
+//   const [render,setRender] = useState(true)
+
+//   useEffect(()=>{
+//     setInterval(()=>{
+//       setRender(r => !r) // why arrow function is used here? what will happen if we use setRender(!render) instead of setRender(r => !r) ?
+//     }, 5000)
+//   },[])
+
+//   return (
+//     <>
+//     {render ? <MyComponent/> : <h2>NAHI HAI <KUCH></KUCH></h2> }
+//     </>
+//   )
+// }
+
+// function MyComponent() {
+//   useEffect(()=>{
+//     console.log('Component mounted')
+//     return () => {
+//       console.log('Component unmounted')
+//     }
+//   }, [])
+
+//   return (
+//     <h1>My Component</h1>
+//   )
+// }
+
+// export default App
+
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+function useTodos(n) {
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   axios.get("https://sum-server.100xdevs.com/todos")
+    //     .then(res => {
+    //       setTodos(res.data.todos);
+    //       setLoading(false);
+    //     })
+    // },1000)
+    setLoading(true);
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+        .then(res => {
+          setTodos(res.data.todos);
+          setLoading(false);
+        })
+    },1000 * n )
+
+    axios.get("https://sum-server.100xdevs.com/todos")
+    .then(res => {
+      setTodos(res.data.todos);
+      setLoading(false);
+    })
+
+    return () => {
+      clearInterval(value);
+    }
+  }, [n])
+
+  return {todos, loading};
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {todos, loading} = useTodos(2);
+  if(loading) {
+    return <h1>Loading...</h1>
+  }
   return (
     <>
-     
+      {todos.map(todo => <Track todo={todo} />)}
     </>
   )
+}
+
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
 }
 
 export default App
